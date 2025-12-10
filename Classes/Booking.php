b30daeda17fa49e83eef918f6702f08e765048f4
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-10 12:30:39 +0100

    feat: adding new public function to get all reservations for a specific movie

--- Classes/Booking.php
+++ Classes/Booking.php
@@ -7,4 +7,12 @@ Class Booking extends db{
         $stmt->execute();
         return $stmt->fetchAll();
     }
+    public function getMovieReservation($movie_id){
+        $query = "SELECT * FROM booking WHERE show_id = :movie_id";
+        $stmt= $this->connect()->prepare($query);
+        $stmt->bindParam(":movie_id",$movie_id);
+        $stmt = $this->connect()->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
 }

e2b2d679e409fff9944e32a22facafb02e0399d6
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-10 13:27:27 +0100

    feat: adding to get data and reserve movie if all goes good you will return to homepage

--- Includes/Booking.php
+++ Includes/Booking.php
@@ -11,4 +11,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $movie_selected = $_POST["movie_selected"];
     $seat_number = $_POST["seat_number"];
     $user_name = $_POST["user_name"];
+
+    $booking =  $book->reserveMovie($user_name,$movie_selected,$seat_number,$time_selected);
+    
+    if($booking){
+        header("location: ../index.php?reservation=success");
+        exit;
+    }
+
 }
